74e095a62290851ea985849d16278c4cfe8762e1
Michael Spencer <ms53150@gmail.com>
2023-02-16 22:15:05 -0500

    Update Author.jsx

--- components/Author.jsx
+++ components/Author.jsx
@@ -3,6 +3,15 @@ import Image from "next/image";
 
 import { grpahCMSImageLoader } from "../util";
 
+// This is a React component that renders the author's information
+// The component takes an author object as a prop
+// The author object should have the following properties: name, photo, bio
+// The component uses the Image component from Next.js to load the author's photo
+// The component uses Tailwind CSS classes to style the elements
+// The component returns a div element with the following elements inside:
+// - A div element that contains the Image component with the author's photo
+// - A h3 element that contains the author's name
+// - A p element that contains the author's bio
 const Author = ({ author }) => (
 	<div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
 		<div className="absolute left-0 right-0 -top-14">
